55db3c9cc90eba4f78904f8130a95f03182e2390
ju-vargas <ju.rodrigues0100@gmail.com>
2023-12-17 23:00:12 -0300

    neander init

---
+++ Neander/Neander.srcs/sources_1/new/control.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12/17/2023 11:34:27 PM
+-- Design Name: 
+-- Module Name: control - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity control is
+--  Port ( );
+end control;
+
+architecture Behavioral of control is
+
+begin
+
+
+end Behavioral;
---
+++ Neander/Neander.srcs/sources_1/new/datapath.vhd
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12/17/2023 11:38:49 PM
+-- Design Name: 
+-- Module Name: datapath - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity datapath is
+--  Port ( );
+end datapath;
+
+architecture Behavioral of datapath is
+-- declarations ==========================================================================================================================================
+-- signals 
+-- components 
+begin
+-- instantiations ========================================================================================================================================
+-- (port maps)
+
+-- Statements ============================================================================================================================================
+-- (combinational and sequential) 
+end Behavioral;
